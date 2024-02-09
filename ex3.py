2e701c79d4ff2ca879c7fc6d388a250cc24b11ab
Raghav <raghav.adlakha@ucalgary.ca>
2024-02-08 20:33:47 -0700

    3.4

--- ex3.py
+++ ex3.py
@@ -28,3 +28,8 @@ print("\nProfiling third_function:")
 profiler = profile.Profile()
 profiler.runcall(third_function)
 profiler.print_stats(sort='cumulative')
+
+#SECTION 3
+#3.1) A profiler creates a set of statistics that describes how often and for how long various parts of the program executed
+#3.2) A profiler creates a profile with alot of statistics that can provide insight to where code can be optimized and more useful information. Benchmarking uses standardized tests to see how a program performs under certain workloads.
+#3.4) Profiling the functions we get a table that contains different information. The first coloumn has ncalls which is the number of calls to the function, next is tottime which is time spent in function excluding time spent in sub or recursive functions. third collumn is percall which is tottime / ncalls. next is cumtime which is time spent in function as well as sub and recursive functions. next is the percall for cumtime / ncalls. finally is filename which is where the function is in the script. With all that in mind I can say that the execution time will be in the cumtime column.
